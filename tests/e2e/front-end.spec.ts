4a94d28fa379f66a6cd4bb2bf29d72e8b0c6776f
Philipp <philipp@ouun.io>
2024-07-26 22:05:08 +0200

    Playwright: Combine frontend tests

--- tests/e2e/front-end.spec.ts
+++ tests/e2e/front-end.spec.ts
@@ -1,5 +1,5 @@
 import { test } from './setup/e2e-wp-test';
-import { validateHeader, networkActivatePlugin } from './utils/tools';
+import { validateHeader, getRandomAnchor, networkActivatePlugin } from './utils/tools';
 import { flushCache } from './utils/flushCache';
 
 /**
@@ -13,17 +13,17 @@ test.describe.configure({ mode: 'serial' });
 test.beforeAll(async ({ requestUtils }) => {
     // Wait some seconds for the backend test to first run the plugin activation tests
     if (process.env.RUN_ALL_TESTS) {
-        await new Promise((resolve) => setTimeout(resolve, 3000));
+        await new Promise((resolve) => setTimeout(resolve, 2000));
     }
 
-    // Activate the plugin
+    // Go sure that the plugin is activated
     await networkActivatePlugin();
 });
 
 /**
- * Test the front-end of the plugin.
+ * Test Caching of the plugin.
  */
-test.describe('Visitor', () => {
+test.describe('Visitor @ WordPress', () => {
 
     /**
      * We flush the cache via WP-CLI and check the status again.
@@ -104,4 +104,70 @@ test.describe('Visitor', () => {
             await validateHeader(response, 'flags', expectedFlags, false);
         }
     });
+});
+
+/**
+ * Test Multisite Caching of the plugin.
+ */
+test.describe('Visitor @ WordPress Multisite', () => {
+    const sites = 5;
+
+    test ('Check if the plugin is active', async ({ page }) => {
+        for (let i = 1; i <= sites; i++) {
+            const response = await page.goto(`/site${i}/`);
+            await validateHeader(response, 'status', ['miss', 'hit']);
+        }
+    });
+
+    test('Network Caching & Flushing', async ({ page, admin }) => {
+        let matrix = [];
+
+        // For each Multisite
+        for (let i = 1; i <= sites; i++) {
+            // Go to the home page
+            await page.goto(`/site${i}/`);
+
+            // Get random link href
+            const anchor = await getRandomAnchor({page});
+
+            // Get href attribute
+            const href = anchor ? await anchor.getAttribute('href') : null;
+
+            if (anchor) {
+                // Open the link
+                const response = await page.goto(href);
+
+                // Wait for the page to load
+                await page.waitForLoadState();
+
+                // Check if the status is set to miss or hit
+                await validateHeader(response, 'status', ['miss', 'hit']);
+
+                // Reload the same page to check if the status is hit
+                const response2 = await page.reload();
+
+                // Check if the status is set to hit
+                await validateHeader(response2, 'status', 'hit');
+
+                // Get the response header "x-millicache-flags"
+                const flags = await validateHeader(response2, 'flags', null, false);
+
+                // Add tested site to matrix with tested links
+                matrix.push({link: href, flags: flags});
+            }
+        }
+
+        // Flush the network cache
+        await flushCache({ page, admin, network: true});
+
+        // Check pages of the matrix
+        for (let i = 0; i < matrix.length; i++) {
+            const {link} = matrix[i];
+            const response = await page.goto(link);
+
+            // Check if the status is set to miss
+            await validateHeader(response, 'status', 'miss');
+        }
+    });
+
 });
