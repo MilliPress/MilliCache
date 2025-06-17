eddfedf3b053e1c69ecf7fac49bd296a0750d477
Philipp <philipp@ouun.io>
2025-06-17 19:05:12 +0200

    Add e2e test for plugins compatibility with WooCommerce

---
+++ tests/e2e/step9-plugins.spec.ts
@@ -0,0 +1,37 @@
+import { test } from './setup/e2e-wp-test';
+import {runWpCliCommand, validateHeaderAfterReload, flushCache} from "./utils/tools";
+
+test.beforeEach(async () => {
+    await flushCache('*');
+});
+
+test.describe('Step 9: Plugins Compatibility', () => {
+    test('WooCommerce', async ({ page }) => {
+        await runWpCliCommand('plugin install woocommerce -- --activate');
+        await runWpCliCommand('wc product create -- --user="admin" --name="New Product" --type="simple" --regular_price="19.99"');
+
+        // Set MilliCache to ignore WooCommerce cookies
+        await runWpCliCommand('config set MC_CACHE_IGNORE_COOKIES "array(\'sbjs\',\'woocommerce_\',\'wp_\')" -- --raw');
+
+        // Product Page
+        await page.goto('/product/new-product/');
+        await validateHeaderAfterReload(page, 'status', 'hit');
+
+        // Cart
+        await page.goto('/cart/');
+        await validateHeaderAfterReload(page, 'status', 'bypass');
+
+        // Checkout
+        await page.goto('/checkout/');
+        await validateHeaderAfterReload(page, 'status', 'bypass');
+
+        // My Account
+        await page.goto('/my-account/');
+        await validateHeaderAfterReload(page, 'status', 'bypass');
+
+        await runWpCliCommand('plugin uninstall woocommerce -- --deactivate');
+
+        // Reset ignore cookies to the original value
+        await runWpCliCommand(`config set MC_CACHE_IGNORE_COOKIES '[]' -- --raw`);
+    });
+});
