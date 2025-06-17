362ba223c79202a17fe1f7663a224a8bc476ce4d
Philipp <philipp@ouun.io>
2025-06-17 11:45:18 +0200

    Log starting message for MilliCache E2E tests

--- tests/e2e/step1-plugin_activation.spec.ts
+++ tests/e2e/step1-plugin_activation.spec.ts
@@ -1,6 +1,10 @@
 import { test } from './setup/e2e-wp-test';
 import { networkActivatePlugin, networkDeactivatePlugin } from './utils/tools';
 
+test.beforeAll(async () => {
+    console.log('Starting MilliCache E2E Tests on: ', process.env.WP_BASE_URL);
+});
+
 test.describe('Step 1: Plugin Activation', () => {
     test('Activate the plugin', async () => {
         await networkActivatePlugin();
