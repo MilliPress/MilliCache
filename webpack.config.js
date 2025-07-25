2cf48cade369c4e1cd7da6822a68ec8e30814549
Philipp <philipp@ouun.io>
2025-07-25 16:11:28 +0200

    Move `admin` assets to `assets` directory and update references in configurations and build scripts.

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 module.exports = {
     ...defaultConfig,
     entry: {
-        admin: path.resolve(__dirname, 'admin/js/admin.js'),
-        adminbar: path.resolve(__dirname, 'admin/js/adminbar.js'),
-        settings: path.resolve(__dirname, 'admin/js/settings.js'),
+        admin: path.resolve(__dirname, 'assets/js/admin.js'),
+        adminbar: path.resolve(__dirname, 'assets/js/adminbar.js'),
+        settings: path.resolve(__dirname, 'assets/js/settings.js'),
     }
 };
