f46a046d380733dbc32987db3bb8cac3c5bc3c83
Philipp <philipp@ouun.io>
2025-10-10 16:04:47 +0200

    Add BuddyPress E2E test and include BuddyPress plugin in `.wp-env.json`

--- tests/e2e/step9-plugins.spec.ts
+++ tests/e2e/step9-plugins.spec.ts
@@ -1,7 +1,7 @@
 import { test } from './setup/e2e-wp-test';
 import {runWpCliCommand, validateHeaderAfterReload, clearCache} from "./utils/tools";
 
-test.beforeEach(async () => {
+test.beforeEach(async ({ page }) => {
     await clearCache('*');
 });
 
@@ -33,5 +33,35 @@ test.describe('Step 9: Plugins Compatibility', () => {
 
         // Reset ignore cookies
         await runWpCliCommand(`config set MC_CACHE_IGNORE_COOKIES '[]' -- --raw`);
+    });
+
+    test('BuddyPress', async ({ page }) => {
+        await runWpCliCommand('plugin activate buddypress');
+
+        // Check if the user exists first before trying to create them
+        const userExistsCheck = await runWpCliCommand('user list -- --network --field=user_login --format=csv')
+            .then(output => output.includes('buddytest'));
+
+        if (! userExistsCheck) {
+            await runWpCliCommand('user create buddytest buddytest@example.com -- --role=subscriber --user_pass=password123');
+        }
+
+        // Flush rewrite rules
+        await runWpCliCommand('rewrite flush');
+
+        // Members Directory
+        await page.goto('/members/');
+        await validateHeaderAfterReload(page, 'status', 'hit');
+
+        // Activity Stream
+        await page.goto('/activity/');
+        await validateHeaderAfterReload(page, 'status', 'hit');
+
+        // Member Profile
+        await page.goto('/members/buddytest/');
+        await validateHeaderAfterReload(page, 'status', 'hit');
+
+        await runWpCliCommand('plugin deactivate buddypress');
+
     });
 });
