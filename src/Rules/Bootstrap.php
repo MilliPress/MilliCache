902c5b245b6a2bed3f4daf18c24f8f4e413bc1b0
Philipp <philipp@ouun.io>
2025-12-03 21:35:29 +0100

    docs: Correct docblock indentation in Bootstrap.php

--- src/Rules/Bootstrap.php
+++ src/Rules/Bootstrap.php
@@ -29,7 +29,7 @@ use MilliCache\Deps\MilliRules\Rules;
  * User can create a rule with order 10 that sets TTL for specific paths,
  * overriding the default no-cache behavior because higher order executes last.
  *
- * @since 		1.0.0
+ * @since       1.0.0
  * @package     MilliCache
  * @subpackage  Rules
  * @author      Philipp Wellmer <hello@millipress.com>
