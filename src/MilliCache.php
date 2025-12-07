e74a7dd9463ac915e5daab9de625b7b16f7d6801
Philipp <philipp@ouun.io>
2025-12-07 20:43:34 +0100

    feat(cache): Add `before_delete_post` hook for cache clearing
    - Ensure post cache is cleared before post deletion by introducing `before_delete_post` hook.

--- src/MilliCache.php
+++ src/MilliCache.php
@@ -134,6 +134,7 @@ final class MilliCache {
 	private function define_cache_hooks() {
 		// Fundamental cache clearing hooks.
 		$this->loader->add_action( 'clean_post_cache', $this, 'clear_post_cache' );
+		$this->loader->add_action( 'before_delete_post', $this, 'clear_post_cache' );
 		$this->loader->add_action( 'transition_post_status', $this, 'transition_post_status', 10, 3 );
 
 		// Register options that clear the full site cache.
