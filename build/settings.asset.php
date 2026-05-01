a195851e8b30e032fac7c601a197e4dc08e9ab53
Philipp Wellmer <philipp@ouun.io>
2026-05-01 17:03:11 +0200

    chore: rebuild assets [skip ci]

--- build/settings.asset.php
+++ build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '11c5818d4a223a5d4d88');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '88ccf8badd03d82f6f67');
