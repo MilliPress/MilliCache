acac5ee5acc9bd44d73bbab978f67638d03245c5
MilliPress CI <ci@millipress.dev>
2026-05-01 18:29:09 +0000

    chore: rebuild assets

--- build/settings.asset.php
+++ build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7caa4185c6d905e8cd37');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '38aceea2412cd3933b9a');
