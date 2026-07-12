e2c5fc94c6ae6eaf86b917f6bbf1d5ed2febb52f
Philipp Wellmer <philipp@ouun.io>
2026-07-12 19:39:59 +0200

    build(assets): sync compiled settings bundle for status UI changes

--- build/settings.asset.php
+++ build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '30f3b3a0a72bf53d3564');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e6d8a7b06978fb8f6ffe');
